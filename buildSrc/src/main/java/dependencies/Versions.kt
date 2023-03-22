4b00705095095d4ed9fa286b35b0ec9762b38389
majid2851 <majidbagheri2851@gmail.com>
2023-03-22 12:01:52 +0430

    part7 is completed.

---
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -0,0 +1,40 @@
+package dependencies
+
+object Versions {
+    val gradle = "7.4.2"
+    val compilesdk = 33
+    val minsdk = 21
+    val targetsdk = 33
+    val kotlin = "1.7.21"
+    val ktx = "1.9.0"
+    val dagger = "2.38.1"
+    val nav_components = "2.4.0-alpha10"
+    val material_dialogs = "3.3.0"
+    val room = "2.4.0-alpha04"
+    val appcompat = "1.4.0-beta01"
+    val constraintlayout = "2.1.0"
+    val material_design = "1.5.0-alpha01"
+    val play_core = "1.10.0"
+    val play_services = "19.4.0"
+    val leak_canary = "2.7"
+    val swipe_refresh_layout = "1.2.0-alpha01"
+    val firebase_firestore = "24.4.0"
+    val firebase_analytics = "20.1.0"
+    val firebase_crashlytics = "2.8.0"
+    val firebase_auth = "21.0.1"
+    val espresso_core = "3.4.0"
+    val espresso_idling_resource = "3.4.0"
+    val mockk_version = "1.12.0"
+    val test_runner = "1.4.0"
+    val test_core = "1.4.0"
+    val coroutines_version = "1.5.0"
+    val coroutines_play_services = "1.5.0"
+    val lifecycle_version = "2.4.0-alpha02"
+    val retrofit2_version = "2.9.0"
+    val markdown_processor = "0.1.4"
+    val junit_jupiter_version = "5.8.0"
+    val junit_4_version = "4.13.2"
+    val fragment_version = "1.3.6"
+    val androidx_test_ext = "1.1.3"
+    val crashlytics_gradle = "2.7.1"
+}
